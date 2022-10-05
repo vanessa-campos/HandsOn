4d2a6dcc9f19006faee88987a0774c8a1c7798c6
Ryan Ricardo Rossi <70291872+ryanricardo@users.noreply.github.com>
2022-10-05 19:10:40 -0300

    player take brick; hands player occupied with brick

---
+++ HandsOn/Assets/Scripts/Itens/Brick.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void FunctionDestroyMe()
+    {
+        Destroy(gameObject, 0);
+    }
+}
--- HandsOn/Assets/Scripts/Player/PlayerController.cs
+++ HandsOn/Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Atributtes Hands")]
+    [SerializeField] protected Hands handsOne;
+    [SerializeField] protected Hands handsTwo;
+
+    [Header("Atributtes Proximity Item")]
+    [SerializeField] protected string nameItem;
+    [SerializeField] protected GameObject gameObjectItem;
+
+    [Header("Inputs")]
+    [SerializeField] public bool inputButtonE;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ControllerInputs();
+        ControllerHands();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Item"))
+        {
+            nameItem = other.gameObject.name;
+            gameObjectItem = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Item"))
+        {
+            nameItem = null;
+            gameObjectItem = null;
+        }
+    }
+
+    protected void ControllerHands()
+    {
+        if(inputButtonE && nameItem != null)
+        {
+            switch(nameItem)
+            {
+                case "PICKUP_BRICK":
+                    Brick brick = gameObjectItem.GetComponent<Brick>();
+                    if(!handsOne.occupied && !handsTwo.occupied)
+                    {
+                        handsOne.nameItem = "Brick";
+                        handsTwo.nameItem = "Brick";
+                        handsOne.occupied = true;
+                        handsTwo.occupied = true;
+                        brick.FunctionDestroyMe();
+                    }
+                    
+                break;
+            }
+        }
+    }
+
+    protected void ControllerInputs()
+    {
+        inputButtonE = Input.GetKeyDown(KeyCode.E) ? inputButtonE = true : inputButtonE = false;
     }
 }
+
+[System.Serializable]
+public class Hands
+{
+    public string nameItem;
+    public bool occupied;
+}
+
+
+
+
