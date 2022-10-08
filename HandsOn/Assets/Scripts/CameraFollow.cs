fa02c61be9ff5364a0c024d931ade6d17d37a288
vanessa-campos <70600785+vanessa-campos@users.noreply.github.com>
2022-10-08 13:13:15 -0300

    movement+dash

---
+++ HandsOn/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private float followDelay = 4;
+    PlayerController player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position + offset, Time.deltaTime * followDelay);
+    }
+}
--- HandsOn/Assets/Scripts/Player/PlayerController.cs
+++ HandsOn/Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField] private float moveSpeed;
+	[SerializeField] private float dashSpeed, dashLength = 0.5f, dashCooldown = 1f;
+	
+	private float activeMoveSpeed;
+	private float dashCounter, dashCoolCounter;
+	
+	private Rigidbody2D rb;
+	private Vector2 moveInput; 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Awake(){
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Start() {	
+		activeMoveSpeed = moveSpeed;	
+	}
+
+	private void Update () {		
+		// Inputs
+		Move();
+		Dash();
+	}	
+
+	private void FixedUpdate() {
+	}	
+	
+	private void Move()	{				
+		moveInput.x = Input.GetAxisRaw("Horizontal");
+		moveInput.y = Input.GetAxisRaw("Vertical");		
+		moveInput.Normalize();
+		rb.velocity = moveInput * activeMoveSpeed;
+	}
+	
+	private void Dash()	{
+		if (Input.GetButton("Fire1")) {
+			if (dashCoolCounter <= 0 && dashCounter <= 0) {
+				activeMoveSpeed = dashSpeed;
+				dashCounter = dashLength;
+			}
+		}
+		if (dashCounter > 0)	{
+			dashCounter -= Time.deltaTime;
+			if(dashCounter <= 0) {
+				activeMoveSpeed = moveSpeed;
+				dashCoolCounter = dashCooldown;
+			}
+		}
+		if (dashCoolCounter > 0) { 
+			dashCoolCounter -= Time.deltaTime;
+		}
+	}
 }
