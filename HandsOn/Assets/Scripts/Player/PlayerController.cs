c618d52d5bfcc226f28aef99296e1afe79b637b6
vanessa-campos <70600785+vanessa-campos@users.noreply.github.com>
2022-10-09 09:17:23 -0300

    movement+dash2

--- HandsOn/Assets/Scripts/Player/PlayerController.cs
+++ HandsOn/Assets/Scripts/Player/PlayerController.cs
@@ -5,55 +5,52 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-	[SerializeField] private float moveSpeed;
-	[SerializeField] private float dashSpeed, dashLength = 0.5f, dashCooldown = 1f;
-	
-	private float activeMoveSpeed;
-	private float dashCounter, dashCoolCounter;
+	[SerializeField] private float moveSpeed, dashSpeed;
+	[SerializeField] private LayerMask dashLayerMask;
+	private bool isdashing;
 	
 	private Rigidbody2D rb;
-	private Vector2 moveInput; 
+	private Vector3 moveDir; 
+	// private PlayerAnimator playerAnim;
 
 	private void Awake(){
 		rb = GetComponent<Rigidbody2D>();
-	}
-
-	private void Start() {	
-		activeMoveSpeed = moveSpeed;	
+		// playerAnim = GetComponent<PlayerAnimator>();
 	}
 
 	private void Update () {		
+		
 		// Inputs
 		Move();
 		Dash();
 	}	
 
 	private void FixedUpdate() {
+		
+		// Move
+		rb.velocity = moveDir * moveSpeed;
+		// Dash
+		if(isdashing) {	
+			Vector3 dashPos = transform.position + moveDir * dashSpeed;
+			RaycastHit2D ray = Physics2D.Raycast(transform.position, moveDir, dashSpeed, dashLayerMask);
+			if(ray.collider != null){ 
+				dashPos = ray.point;
+			}
+			rb.MovePosition(dashPos);
+			isdashing = false;		
+		}
 	}	
 	
 	private void Move()	{				
-		moveInput.x = Input.GetAxisRaw("Horizontal");
-		moveInput.y = Input.GetAxisRaw("Vertical");		
-		moveInput.Normalize();
-		rb.velocity = moveInput * activeMoveSpeed;
+		float moveX = Input.GetAxisRaw("Horizontal");
+		float moveY = Input.GetAxisRaw("Vertical");		
+		moveDir = new Vector3(moveX, moveY).normalized;	
+		// playerAnim.PlayMoveAnim(moveDir);
 	}
 	
 	private void Dash()	{
 		if (Input.GetButton("Fire1")) {
-			if (dashCoolCounter <= 0 && dashCounter <= 0) {
-				activeMoveSpeed = dashSpeed;
-				dashCounter = dashLength;
-			}
-		}
-		if (dashCounter > 0)	{
-			dashCounter -= Time.deltaTime;
-			if(dashCounter <= 0) {
-				activeMoveSpeed = moveSpeed;
-				dashCoolCounter = dashCooldown;
-			}
-		}
-		if (dashCoolCounter > 0) { 
-			dashCoolCounter -= Time.deltaTime;
+			isdashing = true;
 		}
 	}
 }
